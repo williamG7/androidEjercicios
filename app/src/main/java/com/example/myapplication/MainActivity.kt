994ab35746a847002ab005839c9822f1d0807679
william.guzman.7e8 <willesgume@gmail.com>
2025-03-14 12:46:44 +0100

    commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,224 @@
+package com.example.myapplication
+
+import androidx.compose.ui.graphics.Color
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlinx.coroutines.InternalCoroutinesApi
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "word",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    ejercicio3(
+                        name = "3",
+                        modifier = Modifier.padding(innerPadding).toString()
+                    )
+                    ejercicio4(
+                        name = "4",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                    CalcularPropina(
+                        name = "5",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@OptIn(InternalCoroutinesApi::class)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    ConstraintLayout(modifier.fillMaxSize()) {
+        val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
+        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow){
+            start.linkTo(boxRed.start)
+            bottom.linkTo(boxRed.top)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen){
+            start.linkTo(boxRed.start)
+            top.linkTo(boxRed.bottom)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan){
+            top.linkTo(boxRed.top)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta){
+            top.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+    }
+}
+
+@Composable
+fun ejercicio3(name: String, modifier: String){
+    Column(Modifier.fillMaxSize()) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFEADDFF))
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Text composable",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "Displays text and follows the recommended Material Design guidelines",
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFD0BCFF))
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Image composable",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "Creates a composable that lays out and draws a given Painter class object.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFB69DF8))
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Row composable",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "A layout composable that places its children in a horizontal sequence.",
+                    textAlign = TextAlign.Justify
+                )
+
+            }
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color(0xFFF6EDFF))
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    "Column composable",
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth(),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    "A layout composable that places its children in a vertical sequence.",
+                    textAlign = TextAlign.Justify
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ejercicio4(name: String, modifier: Modifier = Modifier) {
+    ConstraintLayout(modifier.fillMaxSize()) {
+        val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
+        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed) {
+            top.linkTo(parent.top)
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow) {
+            bottom.linkTo(boxRed.top)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen) {
+            bottom.linkTo(boxRed.top)
+            start.linkTo(boxRed.end)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan) {
+            top.linkTo(boxRed.bottom)
+            end.linkTo(boxRed.start)
+        })
+        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta) {
+            top.linkTo(boxRed.bottom)
+            start.linkTo(boxRed.end)
+        })
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    MyApplicationTheme {
+        Greeting("Android")
+        ejercicio3("4","completado")
+        ejercicio4("compeltado")
+    }
+}
